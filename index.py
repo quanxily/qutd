3fba14e34b82381aaa8b32c70e296390c62e92ec
quanxily <yangquanxi80@gmail.com>
2023-11-09 11:57:40 +0800

    make it better

--- index.py
+++ index.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template,request
 from datetime import datetime
 
+import firebase_admin
+from firebase_admin import credentials, firestore
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -11,7 +16,7 @@ def index():
     x +="<a href=/about>荃喜網頁</a><br>"
     x +="<a href=/welcome?nick=荃喜>歡迎蒞臨</a><br>"
     x +="<a href=/account>mmi密碼</a><br>"
-
+    x +="<a href=/sort>人選之人名單</a><br>"
     return x
 
 @app.route("/mis")
@@ -41,6 +46,18 @@ def account():
         return result
     else:
         return render_template("account.html")
+
+@app.route("/sort")
+def sort():
+    Result = ""
+    db = firestore.client()
+    collection_ref = db.collection("人選之人")    
+    docs = collection_ref.order_by("date",direction=firestore.Query.DESCENDING).get()   
+    for doc in docs:         
+        Result += "文件內容：{}".format(doc.to_dict()) + "<br>"    
+    return Result
+
+
 
 #if __name__ == "__main__":
    #app.run()
